0e09cfde288ed859ce139e5620fe0288235650ed
EinsteinYap <einsteinyap2014@gmail.com>
2023-08-07 15:14:14 +0800

    updated app.js , npm installed @vueform

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,13 +4,21 @@ import {createApp} from 'vue'
 import {store} from './store/store'
 
 import Departments from './components/Departments.vue';
+import PermissionsCreate from './components/permissions/PermissionsCreate.vue';
+import Users from './components/users/Users.vue';
 
 import Form from 'vform';
 window.Form = Form;
 
+import Multiselect from '@vueform/multiselect';
+import '@vueform/multiselect/themes/default.css';
+
 const app = createApp({})
 
 app.component('departments', Departments);
+app.component('permissions-create', PermissionsCreate);
+app.component('users', Users);
+app.component('multi-select', Multiselect);
 
 window.url = '/task_mis/'
 

3ea243a63103f791698daf2f60a2a48852ea1d89
EinsteinYap <einsteinyap2014@gmail.com>
2023-08-08 15:01:38 +0800

    done implement the logout components and other MVC components

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ require('./bootstrap');
 import {createApp} from 'vue'
 import {store} from './store/store'
 
+import LogoutComponent from './components/auth/LogoutComponent.vue';
 import Departments from './components/Departments.vue';
 import PermissionsCreate from './components/permissions/PermissionsCreate.vue';
 import Users from './components/users/Users.vue';
@@ -15,6 +16,7 @@ import '@vueform/multiselect/themes/default.css';
 
 const app = createApp({})
 
+app.component('logout-component', LogoutComponent)
 app.component('departments', Departments);
 app.component('permissions-create', PermissionsCreate);
 app.component('users', Users);
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -10,17 +10,27 @@
     <link rel="stylesheet" href="{{ asset('public/css/app.css') }}">
 </head>
 <body>
-    
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 mt-5">
-                <img src="{{ asset('public/images/auth.png') }}" alt="auth" width="100%">
-            </div>
-            <div class="col-md-4">
-                @yield('content')
+
+    <div class="auth-main">
+        <div class="auth-container">
+            <div class="container">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-md-6 d-md-block d-none">
+                        <img src="{{ asset('public/images/auth.png') }}" alt="auth" width="100%">
+                    </div>
+                    <div class="col-md-6">
+                        @if(Session::has('success-message'))
+                            <p class="text-success text-center">
+                                {{Session::get('success-message')}}
+                            </p>
+                        @endif
+                        @yield('content')
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+    
 
 </body>
 </html>
